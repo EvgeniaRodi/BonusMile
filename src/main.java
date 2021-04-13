282a4326e6d8c5d7720d4b682e981c9e580b8f32
Evgenia Rodionova <jenechkajj353@gmail.com>
2021-04-13 12:38:37 +0300

    new

--- src/main.java
+++ src/main.java
@@ -1,8 +1,8 @@
 public class main {
-    public static void main(String[] args) {
+    public static void main( String[] args ) {
         long ticketPrice = 2136;
         int bonusMile = 20;
         long TotalBonus = ticketPrice / bonusMile;
-        System.out.println(TotalBonus);
+        System.out.println( TotalBonus );
     }
 }
